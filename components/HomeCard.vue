02974262c548eceea1fc784ef60390d4ed07b0f5
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-05 08:53:36 -0300

    create component homeCard

---
+++ components/HomeCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="home.images[0]" :alt="home.title" style="width: 200px" /><br />
+    {{ home.title }}<br />
+    {{ home.location.adress }} {{ home.location.city }} {{ home.location.state
+    }}<br />
+    {{ home.guests }} guests <br />
+    {{ home.pricePerNight }} / night<br />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    Hello world
+    <div v-for="home in homes" :key="home.objectID" style="float:left; margin: 10px">
+      <HomeCard :home="home" />
+    </div>
   </div>
 </template>
 
 <script>
+import homes from '@/data/homes'
   export default {
-    
+    data:() => ({
+      homes: homes.slice(0,3)
+    })
   }
 </script>
-
 <style lang="scss" scoped>
 
 </style>
