4bd8d1477cb56100f54376385aed1b7eedaa629d
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-16 00:03:42 -0300

    render map

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -20,25 +20,48 @@
     <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }}
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds,
     {{ home.bathrooms }} bath<br />
+    <div style="width: 800px; height:800px;" ref="map"></div>
   </div>
 </template>
 
 <script>
 import homes from '@/data/homes';
 export default {
-  layout: "red",
+  layout: 'red',
   head() {
     return {
-      title: this.home.title
-    }
+      title: this.home.title,
+      script: [
+        {
+          src: `https://maps.googleapis.com/maps/api/js?key=AIzaSyDuHhQv10LzPxrDla3XGZSB4Uc3RgaH0BY&libraries=places"`,
+          hid: 'map',
+          defer: true
+        },
+      ],
+    };
   },
 
   data: () => ({
-    home: {},
+    home: {}
   }),
+
+  mounted() {
+    const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
+
+    const mapOptions = {
+      zoom: 18,
+      center: position,
+      disableDefaultUI: true,
+      zoomControl: true
+    }
+
+    const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+    const marker = new window.google.maps.Marker({ position })
+    marker.setMap(map)
+  },
+
   created() {
     this.home = homes.find((home) => home.objectID === this.$route.params.id);
-    console.log(this.home);
   },
 };
 </script>
