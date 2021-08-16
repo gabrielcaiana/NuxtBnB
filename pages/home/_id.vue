5cf960c95ff23dca07dfc0a31b14ef24381fe439
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-15 21:18:26 -0300

    render images

---
+++ pages/home/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div style="display:flex;">
+      <img
+       v-for="(image, index) in home.images" 
+       :key="index"
+       :src="image" 
+       alt="somenthing descriptive..."
+       width="200"
+       height="150"
+      >
+    </div>
+    {{ home.title }}<br />
+    ${{home.pricePerNight}}<br />
+    {{home.location.address}} {{ home.location.city}} {{home.location.state}} {{home.location.country}}<br />
+    {{home.reviewValue}}
+    {{home.guests}} guests, {{home.bedrooms}} rooms, {{home.beds}} beds, {{home.bathrooms}} bath<br />
+  </div>
+</template>
+
+<script>
+import homes from '@/data/homes'
+  export default {
+    data:() => ({
+      home: {}
+    }),
+    created() {
+      this.home = homes.find(home => home.objectID === this.$route.params.id)
+      console.log(this.home)
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
