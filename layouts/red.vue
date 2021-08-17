91c83567be772a6f4dd91e394b232e4ee42b5164
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-17 19:53:40 -0300

    update router home

--- layouts/red.vue
+++ layouts/red.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header style="background-color: red;">
-      <nuxt-link to="/home">Home</nuxt-link>
+      <nuxt-link to="/">Home</nuxt-link>
     </header>
     <nuxt />
   </div>
