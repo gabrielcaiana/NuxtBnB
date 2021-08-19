ce7cb47474fb1c85c7eab4a94c500c1e81bb4c25
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-08-19 06:54:26 -0300

    create page 404

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="text-align: center;">
+    <img
+      src="https://media1.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=ecf05e471vkmmgcyp41ti443gy7a0qryafj1mh7i473zjs8m&rid=giphy.gif&ct=g"
+      alt="error 404"
+    />
+    <h1>Ooooops!</h1>
+    <p>{{ error.message }} {{ error.statusCode }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    title: "big problems!"
+  }),
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
