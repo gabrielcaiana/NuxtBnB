46a305a929c51d4534d7fb5de3eb3fcf7c786011
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-30 14:49:02 -0300

    the dev bug fix

--- pages/search.vue
+++ pages/search.vue
@@ -16,6 +16,15 @@
   export default {
     watchQuery: ['lat'],
 
+      async beforeRouteUpdate(to, from, next) {
+        const data = await this.$dataApi.getoHomeByLocation(to.query.lat, to.query.lng)
+        this.homes = data.json.hits
+        this.label = to.query.label
+        this.lat = to.query.lat
+        this.lng = to.query.lng
+        next()
+      },
+
     async asyncData({query, $dataApi}) {
       const data = await $dataApi.getoHomeByLocation(query.lat, query.lng)
       return {
