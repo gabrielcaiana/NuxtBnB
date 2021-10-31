437bf9c549d809e7907abf08a46997afc16d8abf
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-31 20:15:44 -0300

    fix user avatar

--- components/User.vue
+++ components/User.vue
@@ -2,7 +2,7 @@
   <div class="app-user-menu">
     <img src="/images/icons/house.svg" alt="house" />
     <div class="name">Host</div>
-    <img src="images/user.jpg" class="avatar" alt="avatar" />
+    <img src="/images/user.jpg" class="avatar" alt="avatar" />
   </div>
 </template>
 
