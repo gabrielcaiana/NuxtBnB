8b29307f53ea2c9f4cfdc176a14d906332604a55
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-31 20:15:08 -0300

    fix: search autocomplete

--- components/Search.vue
+++ components/Search.vue
@@ -15,5 +15,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    this.$maps.makeAutoComplete(this.$refs.citySearch);
+  },
+
+  methods: {
+    changed(event) {
+      const place = event.detail;
+
+      if (!place.geometry) return;
+
+      this.$router.push({
+        name: 'search',
+        query: {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+          label: this.$refs.citySearch.value,
+        },
+      });
+    },
+  },
+};
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,26 +6,5 @@
 </template>
 
 <script>
-  export default {
-    mounted() {
-      this.$maps.makeAutoComplete(this.$refs.citySearch)
-    },
-
-    methods: {
-      changed(event) {
-        const place = event.detail
-
-        if(!place.geometry) return
-
-        this.$router.push({
-          name: 'search',
-          query: {
-            lat: place.geometry.location.lat(),
-            lng: place.geometry.location.lng(),
-            label: this.$refs.citySearch.value
-          }
-        })
-      }
-    },
-  }
+  export default {}
 </script>
