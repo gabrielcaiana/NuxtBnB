c05fcba506fb7f91b59444485e4e6c3df4d6f61a
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-11-04 19:53:28 -0300

    update error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,9 @@
 <template>
-  <div style="text-align: center;">
+  <div class="flex flex-col items-center justify-center">
     <img
       src="https://media1.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif?cid=ecf05e471vkmmgcyp41ti443gy7a0qryafj1mh7i473zjs8m&rid=giphy.gif&ct=g"
       alt="error 404"
+      class="w-2/6"
     />
     <h1>Ooooops!</h1>
     <p>{{ error.message }} {{ error.statusCode }}</p>
@@ -12,7 +13,7 @@
 <script>
 export default {
   head: () => ({
-    title: "big problems!"
+    title: 'big problems!',
   }),
   props: {
     error: {
