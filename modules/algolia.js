dab9b3b961baf97ecdf039bb9e259875762d0690
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-11-26 08:24:22 -0300

    chore: finish proxy algolia

--- modules/algolia.js
+++ modules/algolia.js
@@ -13,10 +13,17 @@ export default function () {
     app.use('/api/user', getUserRoute)
   })
 
-  function getUserRoute(req, res, next) {
-    console.log(req.identity)
+  async function getUserRoute(req, res, next) {
+    const identity = req.identity
+    const userData = await getUserById(identity.id)
+
+    if(userData.status == 200) {
+      sendJson(userData.json, res)
+      return
+    }
+
     createUser(req.identity)
-    next()
+    sendJson(makeUserPayload(identity), res)
   }
 
   async function createUser(identity) {
@@ -31,6 +38,23 @@ export default function () {
       } catch (error) {
         return getErrorResponse(error);
       }
+  }
+
+  async function getUserById(userId) {
+    try {
+      return unWrap(
+        await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${userId}`, {
+          headers
+        })
+      );
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
+
+  function sendJson(data, res) {
+    res.setHeader('content-Type', 'application/json')
+    res.end(JSON.stringify(data))
   }
 
   function makeUserPayload(identity) {
