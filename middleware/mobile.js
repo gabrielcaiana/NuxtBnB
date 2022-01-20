fe1e1b0ffd0cee53cdfb7ada0955bbf83c03e618
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-19 23:36:56 -0300

    chore: remove mobile middleware

--- middleware/mobile.js
+++
@@ -1,6 +0,0 @@
-export default function(context) {
-  let userAgent = process.server ? context.req.headers['user-agent'] : navigator.userAgent
-  let isMobile =  /mobile/i.test(userAgent)
-  context.isMobile = isMobile
-  context.store.commit('device/setMobile',  isMobile)
-}
